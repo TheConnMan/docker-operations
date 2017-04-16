f1f7ec8795ac9675eaf2fa66b1bbaf1a4810a6e1
TheConnMan <bcconn2112@gmail.com>
2017-04-16 15:20:43 -0400

    Fix stopped container script typos

--- scripts/stopped-container.sh
+++ scripts/stopped-container.sh
@@ -4,6 +4,6 @@
 # */30 * * * * /path/to/stopped-containers.sh <missd-url>
 
 count=`docker ps -f "status=exited" | wc -l`
-if [ "$count" -eq "1"" ]; then
-  curl -X POST $2
+if [ "$count" -eq "1" ]; then
+  curl -X POST $1
 fi
