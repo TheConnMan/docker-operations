d70facd7acc0a276aeec5f8df9fd08ee38b1cd47
TheConnMan <bcconn2112@gmail.com>
2017-04-24 01:15:59 +0000

    Use MySQL user and password instead of root password

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -4,7 +4,7 @@
 # 0 1 * * * /path/to/mysql-backup.sh <mysql-container> <filename> <s3-bucket/folder> <missd-url>
 
 filename=`date +%H-%M-%S`-$2.sql
-docker exec $1 sh -c 'exec mysqldump --all-databases -uroot -p"$MYSQL_ROOT_PASSWORD"' > $filename
+docker exec $1 sh -c 'exec mysqldump --all-databases -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"' > $filename
 location=s3://$3/`date +%Y/%m/%d/`
 /usr/local/bin/aws s3 cp $filename $location
 rm $filename
